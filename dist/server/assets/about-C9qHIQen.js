0521c595143c3ee6a5259f56172affc290361167
King Dav <mickyyoung660@gmail.com>
2026-07-02 22:55:13 +0100

    feat: set up statict deployment

--- dist/server/assets/about-C9qHIQen.js
+++
@@ -1,4 +0,0 @@
-//#region src/assets/about.jpg
-var about_default = "/assets/about-CwO5Xn8v.jpg";
-//#endregion
-export { about_default as t };
--- dist/server/assets/craft-CPON8fan.js
+++
@@ -1,4 +0,0 @@
-//#region src/assets/craft.jpg
-var craft_default = "/assets/craft-CcZhYu3M.jpg";
-//#endregion
-export { craft_default as t };
